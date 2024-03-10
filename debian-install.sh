d5c10c2749f9c0aa2e5e0c27533f17e8ace0ce14
RobertC85 <80091082+RobertC85@users.noreply.github.com>
2024-03-10 17:02:01 -0400

    Update debian-install.sh
    getting the dependencies for dietpi linux sorted as i remember them

--- debian-install.sh
+++ debian-install.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-echo "Not Ready Yet"
+echo "Not Ready Yet, this will be a public open source project soon, but for the love of your sanity please precc ctrl+c now!"
+sleep 15
+sudo apt install -y x11vnc xorg xinit chromium apache2 proftpd vlc 
