375227cebb76ff2cc4e980056da32c5be450a7eb
RobertC85 <80091082+RobertC85@users.noreply.github.com>
2024-03-11 16:17:06 -0400

    Update debian-install.sh
    oopsie, made a typo

--- debian-install.sh
+++ debian-install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo "Not Ready Yet, this will be a public open source project soon, but for the love of your sanity please precc ctrl+c now!"
+echo "Not Ready Yet, this will be a public open source project soon, but for the love of your sanity please press ctrl+c now!"
 sleep 15
 #installing dependencies
 sudo apt install -y x11vnc xorg xinit chromium apache2 proftpd vlc 
