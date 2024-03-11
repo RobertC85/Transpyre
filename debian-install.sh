f683ac7647bbc438a872b3e4229df9da321d7fbd
RobertC85 <80091082+RobertC85@users.noreply.github.com>
2024-03-11 08:55:45 -0400

    Update debian-install.sh

--- debian-install.sh
+++ debian-install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 echo "Not Ready Yet, this will be a public open source project soon, but for the love of your sanity please precc ctrl+c now!"
 sleep 15
+#installing dependencies
 sudo apt install -y x11vnc xorg xinit chromium apache2 proftpd vlc 
