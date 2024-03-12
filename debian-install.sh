1f5d09ef6fef1da8fd498bada0425217c50a372d
RobertC85 <80091082+RobertC85@users.noreply.github.com>
2024-03-12 00:15:21 -0400

    Update debian-install.sh
    first beta stage update, version 20240312a

--- debian-install.sh
+++ debian-install.sh
@@ -9,3 +9,12 @@ echo "okay you glutton for punishment by pre-alpha software testing you, here we
 sudo cp -r ~/transpyre/rootFS/* /
 sudo a2enmod cgid
 echo "ok if we are still alive you can login with your browser to your devices IP to access your options, i'm going to add VNC here in a moment"
+#write out current crontab
+crontab -l > mycron
+#echo new cron into cron file
+echo "@reboot /usr/local/sbin/vnclauncher" >> mycron
+echo "@reboot /usr/local/sbin/startup" >> mycron
+echo "* * * * * /var/www/html/cgi-bin/temp.sh" >> mycron
+#install new cron file
+crontab mycron
+rm mycron
