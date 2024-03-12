6253155981eaa1b5749097b90721c881f7ccdc79
RobertC85 <80091082+RobertC85@users.noreply.github.com>
2024-03-12 08:53:42 -0400

    Update debian-install.sh
    ADDED FEATURE, SCHEDULING!!!
    still in testing of course

--- debian-install.sh
+++ debian-install.sh
@@ -2,7 +2,7 @@
 echo "Not Ready Yet, this will be a public open source project soon, but for the love of your sanity please press ctrl+c now!"
 sleep 15
 #installing dependencies
-sudo apt install -y x11vnc xorg xinit chromium-browser apache2 proftpd vlc 
+sudo apt install -y x11vnc xorg xinit chromium-browser apache2 proftpd vlc npm nodejs
 echo "okay i got that part, this is the last step before we start making real changes and installing our software to your device, i'll give you a few seconds to change your mind"
 sleep 5
 echo "okay you glutton for punishment by pre-alpha software testing you, here we go"
@@ -19,3 +19,10 @@ echo "* * * * * /var/www/html/cgi-bin/temp.sh" >> mycron
 crontab mycron
 rm mycron
 sudo service apache2 restart
+#INSTALLING CRONICLE SCHEDULE MANAGER
+mkdir -p /opt/cronicle
+cd /opt/cronicle
+curl -L https://github.com/jhuckaby/Cronicle/archive/v1.0.0.tar.gz | tar zxvf - --strip-components 1
+npm install
+node bin/build.js dist
+# CRONICLE IS MIT LICENSED SO SHOULD BE ABLE TO BE ADDED HERE.
